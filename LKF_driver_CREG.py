21ad3fe3efe10c48f5063433af84c6e485f4f571
Jean-Francois Lemieux <jean-francois.lemieux@canada.ca>
2022-09-26 13:55:22 +0000

    Added code for plotting LKFs

--- LKF_driver_CREG.py
+++ LKF_driver_CREG.py
@@ -3,6 +3,8 @@
 
 import xarray as xr
 import os
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 from pathlib import Path
 from lkf_tools.dataset import *
 
@@ -15,6 +17,7 @@ ddate='2005032903_000'
 main_dir='/home/jfl001/data/runsLemieux_et_al_2022/'
 main_dir_grid='/home/socn000/data/eccc-ppp5/env_rhel-8-icelake-64/datafiles/constants/oce/repository/master/CONCEPTS/'
 store_main_dir='/home/jfl001/data/Lemieux2022/LKF_diag'
+produce_plot=True
 
 #----- define paths and file name --------
 
@@ -51,7 +54,37 @@ creg_nc = xr.Dataset.merge(creg_nc, grid_nc)
 #creg_nc = creg_nc.rename({'ni':'x', 'nj':'y','divu':'div', 'shear':'shr', 'aice':'A', 
 #                          'uvel':'U', 'vvel':'V', 'TLON':'ULON', 'TLAT':'ULAT'})
 
+#---- process data and detect LKFs ---
+
 lkf_data = process_dataset(fileout,creg=cregflag,
                            output_path=store_path, xarray=creg_nc, t_red=1)
 
 lkf_data.detect_lkfs(indexes=[0])
+
+#---- plot LKFs ---------------------
+
+if (produce_plot):
+
+    fig = plt.figure(figsize=[10, 5])
+
+    ax = plt.subplot(1, 1, 1, projection=ccrs.Orthographic(0, 90))
+
+    ax.coastlines(zorder=3)
+
+    pcm = ax.pcolormesh(lkf_data.lon[max([0,lkf_data.index_y[0][0]-1]):lkf_data.index_y[0][-1]+2:lkf_data.red_fac,
+                                     max([0,lkf_data.index_x[0][0]-1]):lkf_data.index_x[0][-1]+2:lkf_data.red_fac],
+                        lkf_data.lat[max([0,lkf_data.index_y[0][0]-1]):lkf_data.index_y[0][-1]+2:lkf_data.red_fac,
+                                     max([0,lkf_data.index_x[0][0]-1]):lkf_data.index_x[0][-1]+2:lkf_data.red_fac],
+                        np.sum(lkf_data.eps_tot_list,axis=0),transform=ccrs.PlateCarree(),vmin=0,vmax=1e-1,cmap='Greys_r')
+
+    it = lkf_data.indexes[-1]
+
+    lkfs = np.load(lkf_data.lkfpath.joinpath('lkf_%s_%03i.npy' %(lkf_data.netcdf_file.split('/')[-1].split('.')[0],(it+1))),allow_pickle=True)
+
+    for ilkf in lkfs:
+        if np.min(ilkf[:,2])<-150 and np.max(ilkf[:,2]>150):
+            ilkf[ilkf[:,2]<0,2]+=360
+        ax.plot(ilkf[:,2],ilkf[:,3],transform=ccrs.PlateCarree())
+
+    plt.colorbar(pcm,label='total deformation')
+    plt.savefig('testing12.png')
