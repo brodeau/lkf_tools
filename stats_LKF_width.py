bf8a0a0b5fb96df3840f91c349ddbab70752b4de
Jean-Francois Lemieux <jean-francois.lemieux@canada.ca>
2022-10-20 18:46:16 +0000

    Minor modifs to stats_LKF_width.py

--- stats_LKF_width.py
+++ stats_LKF_width.py
@@ -13,7 +13,7 @@ EXP='run6f'
 SDATE='20050329'
 EDATE='20050329'
 main_dir='/home/jfl001/data/Lemieux2022/LKF_diag'
-mybins=np.linspace(0, 8, 100)
+mybins=np.linspace(0, 15, 100)
 
 #----- define paths and file names --------
 
@@ -27,14 +27,24 @@ path_filein2=os.path.join(main_dir+'/'+creggrid+'/'+EXP+'/WIDTH/'+filein2)
 hwidth1 = np.load(path_filein1,allow_pickle=True)
 hwidth2 = np.load(path_filein2,allow_pickle=True)
 
-plt.hist(hwidth1, bins=mybins, color = "dodgerblue", ec="dodgerblue")
-plt.hist(hwidth2, bins=mybins, alpha=0.3, color = "magenta", ec="magenta")
+print(hwidth1.shape[0])
+print(hwidth2.shape[0])
+npoints=hwidth1.shape[0]
+width=np.zeros(npoints)
+width[:]=hwidth1[:]+hwidth2[:]
+print('mean values for hw1, hw2 and width are:')
+print(np.mean(hwidth1))
+print(np.mean(hwidth2))
+print(np.mean(width))
+
+plt.hist(width, bins=mybins, color = "dodgerblue", ec="dodgerblue")
+#plt.hist(hwidth2, bins=mybins, alpha=0.3, color = "magenta", ec="magenta")
 #plt.hist(Ddef2, bins=mybins, alpha=0.3, color = "orange", ec="orange", )
 #plt.yscale('log')
 #plt.xlabel(r'$\Delta \dot{\epsilon_{II}}$', fontsize=14)
 #plt.ylabel('Nb of counts', fontsize=12)
 #plt.legend([label1, label2], loc ="upper right", markerscale=1)
-plt.savefig('hwidth2.png')
+plt.savefig('width.png')
 
 
 
