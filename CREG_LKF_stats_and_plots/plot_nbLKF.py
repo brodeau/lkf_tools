05f7d55690212b032b25662cda93a88876e33d7b
Jean-Francois Lemieux <jean-francois.lemieux@canada.ca>
2024-04-12 14:08:37 +0000

    modif to plot_nbLKF.py to have dates on x axis

--- CREG_LKF_stats_and_plots/plot_nbLKF.py
+++ CREG_LKF_stats_and_plots/plot_nbLKF.py
@@ -5,42 +5,34 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 #----- INPUT -----
-#label1='$P_{ref}$'
-#label2='$P_{ref}$*1.25'
-#label3='$P_{ref}$*1.5'
 #PATH_OUT='/home/jfl001/data/Lemieux2022/LKF_diag/Histo_width'
-main_dir='/home/jfl001/data/Lemieux_et_al_plast_pot/LKF_diag'
-exp1='eg1p0_ef1p5'
-exp2='eg1p5_ef1p5'
-exp3='eg2p25_ef1p5'
-suffix='20050101_20050531.npy'
+main_dir='/home/jfl001/data/LKF_rips_analysis'
+exp1='exp1'
+exp2='control'
+#exp3='eg2p25_ef1p5'
+suffix='20200101_20201230.npy'
 #-----------------
 
 file1=os.path.join(main_dir+'/'+exp1+'/nbLKFs/number_lkf_'+suffix)
 file2=os.path.join(main_dir+'/'+exp2+'/nbLKFs/number_lkf_'+suffix)
-file3=os.path.join(main_dir+'/'+exp3+'/nbLKFs/number_lkf_'+suffix)
+#file3=os.path.join(main_dir+'/'+exp3+'/nbLKFs/number_lkf_'+suffix)
 
 df1 = pd.read_csv(file1)
 df2 = pd.read_csv(file2)
-df3 = pd.read_csv(file3)
+print(df1)
+#df3 = pd.read_csv(file3)
 
 print('mean nb1:', df1['nb_of_LKFS'].mean())
 print('mean nb2:', df2['nb_of_LKFS'].mean())
-print('mean nb3:', df3['nb_of_LKFS'].mean())
+#print('mean nb3:', df3['nb_of_LKFS'].mean())
 
-plt.figure(1)
-plt.plot(df1['nb_of_LKFS'],color = "darkviolet")
-plt.plot(df2['nb_of_LKFS'],color = "dodgerblue")
-plt.plot(df3['nb_of_LKFS'],color = "orange")
-#plt.plot(x, y2, marker='o')
-#plt.plot(x, y3, marker='o')
-#plt.ylim(4, 5)
-#plt.xlim(1.1, 2.1)
-#plt.legend([label1, label2, label3], loc ="lower right")
-plt.ylabel('Number of LKFs',fontsize='14')
-plt.xlabel('time',fontsize='14')
-#plt.xticks(x, mylabels)
-#plt.locator_params(axis='x', nbins=7)
-#plt.xticks(rotation=45)
-#plt.savefig(fileout)
-plt.show()
+ax = df1.plot(x = 'date', y = 'nb_of_LKFS', color = "dodgerblue")
+ax.set_prop_cycle(None)
+df2.plot(ax=ax, x = 'date', y = 'nb_of_LKFS', color = "orange")
+#df3.plot(ax=ax, x = 'date', y = 'nb_of_LKFS', color = "darkviolet")
+ax.legend([exp1, exp2])
+ax.set_xlabel("", fontsize='2')
+ax.set_ylabel("Nb of LKFs")
+
+plt.savefig('Nb_LKFs_2020.png')
+#plt.show()
