777ca0b8f3b07111272a18a9f5a9833545797b24
brodeau <brodeau@gmail.com>
2025-06-05 17:04:22 +0200

    LB

---
+++ nanuk12_driver_LKF_detect.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+##################################################################
+
+import os,sys
+import numpy as np
+import pandas as pd
+from datetime import timedelta
+from CREG_lkf_tools  import *
+import pickle
+import calendar
+
+#----  CREG_driver_LKF_detect -------------------------------
+#
+# Driver that loops through a series of files (dates) and that
+# calls the funtion CREG_lkf_detect.
+#
+#------------------------------------------------------------
+
+#----- INPUT -----
+#ni = 528 ; creg025
+#nj = 735 ;
+#ni = 1580 ; creg12
+#nj = 2198 ;
+cregflag = 1 # 1: output includes vorticity, 2: no vorticity
+CONF = 'NANUK12'
+
+EXP='CPL00'
+
+main_dir = '/data/gcm_setup/'+CONF
+
+main_dir_grid = main_dir+'/'+CONF+'.L31-I'
+
+main_dir_exp  = main_dir+'/'+CONF+'-'+EXP+'_BBM'
+
+
+store_main_dirTP = '/data/gcm_setup/tmp/'+CONF
+
+kvalue = 7 # value for kernel
+produce_plot = True
+pack_ice_mask = False
+SDATE = '19970201'
+EDATE = '19970202'
+FREQ = '3h'
+suffix = 'icemod'
+
+#----- check kernel value ----------------
+# kvalue should be odd: see Nils' email (4 nov 2022)
+
+if kvalue % 2 == 0:
+    print("kernel value should be an odd integer")
+    exit()
+else:
+    print("kernel value = ")
+    print(kvalue)
+
+#----- define paths and file name --------
+
+#if (CONF == 'creg025'):
+#    grid_path=os.path.join(main_dir_grid+'/creg025pe/grid/coordinates_CREG025_LIM.nc')
+#elif (CONF == 'creg12'):
+#    grid_path=os.path.join(main_dir_grid+'/creg012pe/grid/coordinates_CREG12_ext.nc')
+#else:
+#    print ("Wrong choice of grid")
+
+
+
+#grid_path=os.path.join(main_dir_grid+'/mesh_mask_'+CONF+'_L31_4.2_1stlev.nc')
+grid_path=os.path.join(main_dir_grid+'/coordinates_'+CONF+'.nc')
+
+
+if (pack_ice_mask):
+    store_main_dir=store_main_dirTP+'/LKF_diag_pack'
+else:
+    store_main_dir=store_main_dirTP+'/LKF_diag'
+
+store_path=os.path.join(store_main_dir+'/'+EXP+'/detectedLKFs/')
+
+#list_dates=list(pd.date_range(SDATE,EDATE, freq=FREQ))
+
+#for i in range(len(list_dates)) :
+#    date0 = (list_dates[i] + timedelta(days=-0)).strftime('%Y%m%d%H')
+#    data_path=os.path.join(main_dir+'/'+EXP+'/hourly/'+date0+suffix+'.nc')
+#    fileout=date0 + '_' + EXP
+#    print(fileout)
+#    CREG_lkf_detect(date0, CONF, cregflag, grid_path, data_path, store_path, fileout, kvalue, produce_plot, pack_ice_mask)
+
+data_path = main_dir_exp+'/'+CONF+'-'+EXP+'_'+FREQ+'_'+SDATE+'_'+EDATE+'_'+suffix+'.nc4'
+
+list_dates=list(pd.date_range(SDATE,SDATE, freq=FREQ))
+date0 = (list_dates[0] + timedelta(days=-0)).strftime('%Y%m%d%H')
+
+
+print('date0 =', date0)
+
+
+fileout=date0+'_'+CONF+'_'+EXP
+
+CREG_lkf_detect(date0, CONF, cregflag, grid_path, data_path, store_path, fileout, kvalue, produce_plot, pack_ice_mask)
+
+print('Detection done for experiment:')
+print(CONF+'--'+EXP)
